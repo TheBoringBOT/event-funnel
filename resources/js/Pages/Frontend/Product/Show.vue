4c590c54112e77c93b06da0c6c74a4c6be9c514d
TheBoringBot <richardsgareth@icloud.com>
2022-04-27 00:10:19 +0200

    fix: v-else in wrong place

--- resources/js/Pages/Frontend/Product/Show.vue
+++ resources/js/Pages/Frontend/Product/Show.vue
@@ -35,7 +35,6 @@
                                 ></span>
 
                                 <button
-                                    v-else
                                     class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
                                     @click="$store.commit('addToCart', product)"
                                 >
