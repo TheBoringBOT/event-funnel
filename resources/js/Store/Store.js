5a4e02b9e2361e3cd9fad9d518ea69bc9f6e7c7b
TheBoringBot <richardsgareth@icloud.com>
2022-04-27 12:19:17 +0200

    feat: functions for increase/decrease cart item quantity

--- resources/js/Store/Store.js
+++ resources/js/Store/Store.js
@@ -16,17 +16,41 @@ export default {
         },
         addToCart(state, product) {
             // get current id of item in cart
+            const productAvailability = product.available;
+
             let productIdInCartIndex = state.cart.findIndex(
                 (item) => item.slug === product.slug
             );
+
             // check if product if exists in cart and add quantity
             if (productIdInCartIndex !== -1) {
+                // If the amount in cart is equal to the availability of the product then don't add more
+                // TODO  add toast to show no more available
+                if (
+                    productAvailability ===
+                    state.cart[productIdInCartIndex].quantity
+                ) {
+                    return;
+                }
                 state.cart[productIdInCartIndex].quantity++;
                 return;
             }
+            console.table(product);
             product.quantity = 1;
 
             state.cart.push(product);
+        },
+
+        increaseQuantity(state, product) {
+            state.cart[product].quantity++;
+        },
+        decreaseQuantity(state, product) {
+            //removes product via splice if only one quantity
+            if (state.cart[product].quantity === 1) {
+                state.cart.splice(product, 1);
+                return;
+            }
+            state.cart[product].quantity--;
         },
 
         removeFromCart(state, store) {
@@ -40,7 +64,7 @@ export default {
         },
     },
     actions: {
-        getProducts({commit}) {
+        getProducts({ commit }) {
             //fetch the product from the api api/products
             //TODO: update catch error with something better for customer
             axios
@@ -51,7 +75,7 @@ export default {
                 .catch((error) => console.log(error));
         },
         // clear/empty out the cart
-        clearCart({commit}) {
+        clearCart({ commit }) {
             commit("updateCart", []);
         },
     },
